8d24d37fc9fcb42c258629c1f2bace11cee7a6f5
fluffy-unicorn <matthijsvhelden@gmail.com>
2014-05-06 19:42:02 +0200

    Latest versions

---
+++ session2/result3e.sql
@@ -0,0 +1,9 @@
+﻿SELECT p.name, min(m.year) AS year FROM Person p
+JOIN Directs d ON p.pid = d.pid
+JOIN Movie m ON d.mid = m.mid
+JOIN Genre g ON m.mid = g.mid
+WHERE g.genre = 'Action'
+GROUP BY p.pid
+HAVING COUNT(*) >= 2
+
+;
---
+++ session2/result3f.sql
@@ -0,0 +1,6 @@
+﻿SELECT p.name, COUNT(*) FROM Person p 
+JOIN Acts a ON p.pid = a.pid
+JOIN Movie m ON m.mid = a.mid
+--where rating > 9.0
+GROUP BY p.pid
+HAVING COUNT(*) >= 4;
