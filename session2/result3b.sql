3245754cdd4a30cf7fb589263fae3423146c999d
fluffy-unicorn <matthijsvhelden@gmail.com>
2014-05-06 16:00:03 +0200

    Initial step, forgot an 's' :/

--- session2/result3b.sql
+++ session2/result3b.sql
@@ -1,10 +1,8 @@
-﻿SELECT p.pid FROM Person p
-WHERE EXIST(
-	SELECT * FROM Acts a
-	WHERE a.pid = p.pid
-	AND EXIST (
-		SELECT * FROM Movie m
-		WHERE m.mid = a.mid
-		AND m.name = 'Back to the Future'
-	)
+﻿SELECT p.pid FROM Person p 
+WHERE EXISTS(
+SELECT * FROM Acts a 
+WHERE a.pid = p.pid AND EXISTS (
+SELECT * FROM Movie m 
+WHERE m.mid = a.mid AND m.name = 'Back to the Future'
+)
 );
