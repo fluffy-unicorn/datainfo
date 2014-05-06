21ed8f2862564328fbbdd83005fe48fe3742c91a
fluffy-unicorn <matthijsvhelden@gmail.com>
2014-05-06 15:31:18 +0200

    Exercise 2.1

---
+++ session2/result2.sql
@@ -0,0 +1,3 @@
+INSERT INTO F VALUES('ABC','EF',false,'Met ABC weet je EF een daardoor ook D');
+INSERT INTO F VALUES('E','D',true,'E is geen superkey en E->D is niet triviaal');
+INSERT INTO F VALUES('D','A',true,'D is geen superkey en D->A is niet triviaal');
