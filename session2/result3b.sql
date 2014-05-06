2e5a6ae60397281e929d1bb7d38f2e3094f8c274
fluffy-unicorn <matthijsvhelden@gmail.com>
2014-05-06 15:55:10 +0200

    initial step

---
+++ session2/result3b.sql
@@ -0,0 +1,10 @@
+﻿SELECT p.pid FROM Person p
+WHERE EXIST(
+	SELECT * FROM Acts a
+	WHERE a.pid = p.pid
+	AND EXIST (
+		SELECT * FROM Movie m
+		WHERE m.mid = a.mid
+		AND m.name = 'Back to the Future'
+	)
+);
